2fe9cdc77f45a55e8ab196e6eb52f219c643c37e
sardaarNiamotullah <sardaar.niamotullah@gmail.com>
2025-07-01 20:14:04 +0600

    feat(groq): enhance query prompt for better responses
    - Improved clarity and specificity in the Grok query prompt
    - Optimized structure to reduce ambiguity in responses
    - Added contextual hints for more relevant outputs

--- apps/itinerary/services/prompt_builder.py
+++ apps/itinerary/services/prompt_builder.py
@@ -15,8 +15,8 @@ def build_itinerary_prompt(destination, weather_data):
         "- Suggested clothing\n"
         "- Local foods to try\n\n"
         "Make it helpful and friendly for a traveler.\n\n"
-        "Your Response shoud fllow this manner [[Day 1 data],[Day 2 data],[Day 3 data],] i.e one parent array and 3 child array inside of it containing the initiraty about each day. \n\n"
-        "DO NOT RESPONSE WITH ANYTHING OUTSIE OF ARRAY."
+        'Your Response shoud fllow this manner [{"day_1": "data"},{"day_2": "data"}, {"day_3": "data"}] i.e it should be json format where each day is a key. \n\n'
+        "DO NOT RESPONSE WITH ANYTHING OUTSIE OF JSON format."
     )
 
 def build_followup_prompt(itinerary_data: str) -> str:
@@ -27,6 +27,7 @@ def build_followup_prompt(itinerary_data: str) -> str:
         "The user has shared the following itinerary data. "
         "Keep this context in mind when replying:\n\n"
         f"{itinerary_data}\n\n"
+        "And there is already a messge on behafe of you to the user in a hard coded manner, which is: Do you have anything in mind regarding my generated travel itinerary for you? So, Don't act like you are talking to the user for the very first time."
         "Be very precise and on the point. However, sound friendly at the same time."
     )
 
